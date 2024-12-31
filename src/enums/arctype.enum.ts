d1e2e7a7fdbb6ded7c79a96efc7c537352bfaf72
Lucas Schraier <lucas.schraier@shipsigma.com>
2024-12-30 20:56:02 -0500

    Add anomaly and competency enum types

--- src/enums/arctype.enum.ts
+++ src/enums/arctype.enum.ts
@@ -3,3 +3,27 @@ export enum ArcType {
     Competency='Competency',
     Reality='Reality',
 }
+
+export enum CompetencyType {
+    PR='PR',
+    RnD='R&D',
+    Barista='Barista',
+    CEO='Ceo',
+    Intern='Intern',
+    Gravedigger='Gravedigger',
+    Reception='Reception',
+    Hotline='Hotline',
+    Clown='Clown'
+}
+
+export enum AnomalyType {
+    Absence='Absence',
+    Catalogue='Catalogue',
+    Drain='Drain',
+    Dream='Dream',
+    Growth='Growth',
+    Gun='Gun',
+    Manifold='Manifold',
+    Timepiece='Timepiece',
+    Whisper='Whisper'
+}
